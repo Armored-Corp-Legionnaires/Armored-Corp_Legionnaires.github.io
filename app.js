38c4d92c9ab80ba9b19e01a815b3a8f7b13045d5
Jeremy <55060352+Arckadian@users.noreply.github.com>
2020-10-16 11:00:25 -0600

    Added extra line at end of file.

---
+++ app.js
@@ -0,0 +1,10 @@
+//Comments will work like you remember.  Just remember to use them.
+
+'use strict'
+
+let today = new Date();
+let formatDate = today.toDateString();
+let sleectElement = document.getElementById('date');
+selectElement.innerHTML = formatDate;
+
+console.log('Testing point')
